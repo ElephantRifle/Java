d4c2c052b97398b597658541c4ffd14cb8c8b0ab
ElephantRifle <dhirendrakumar300200@gmail.com>
2025-04-24 23:29:22 +0530

    Inheritance

--- 1st/Exam.java
+++ 1st/Exam.java
@@ -27,36 +27,163 @@
 
 //OVERRIDING
 
-class Animal{
-    void speaks(){
-        System.out.println("Animal Makes a Sound.");
-    }
-    void eats(){
-        System.out.println("Animal Eats Food.");
-    }
-}
+// class Animal{
+//     void speaks(){
+//         System.out.println("Animal Makes a Sound.");
+//     }
+//     void eats(){
+//         System.out.println("Animal Eats Food.");
+//     }
+// }
 
-class Dog extends Animal{
-    void speaks(){
-        System.out.println("Dog Barks..");
-    }
-    void eats(){
-        System.out.println("Dog Eats Eags..");
-    }
-}
+// class Dog extends Animal{
+//     void speaks(){
+//         System.out.println("Dog Barks..");
+//     }
+//     void eats(){
+//         System.out.println("Dog Eats Eags..");
+//     }
+// }
 
-class Exam{
-    public static void main (String args[]){
-        Animal animal = new Animal();
-        Dog dog = new Dog();
+// class Exam{
+//     public static void main (String args[]){
+//         Animal animal = new Animal();
+//         Dog dog = new Dog();
 
-        System.out.println("====  Animal  ====");
-        animal.speaks();
-        animal.eats();
+//         System.out.println("====  Animal  ====");
+//         animal.speaks();
+//         animal.eats();
 
-        System.out.println("====  Dog  ====");
-        dog.speaks();
-        dog.eats();
+//         System.out.println("====  Dog  ====");
+//         dog.speaks();
+//         dog.eats();
 
-    }
-}
+//     }
+// }
+
+
+//POLYMORPHISM
+
+// class Animal{
+//     void speaks(){
+//         System.out.println("Animal Speaks..");
+//     }
+//     //METHOD OVERLOADING
+//     void eats(){
+//         System.out.println("Animal Eats..");
+//     }
+//     void eats(String  a){
+//         System.out.println("Animal Eats "+ a);
+//     }
+// }
+// class Dog extends Animal{
+//     @Override
+//     //OVERRIDING
+//     void speaks(){
+//         System.out.println("Dog Barks..");
+//     }
+// }
+// class Exam {
+//     public static void main (String args[]){
+//         Animal a = new Animal();
+//         Dog b = new Dog();
+//         a.speaks();
+//         a.eats();
+//         a.eats("Grass");
+
+//         b.speaks();
+
+//     }
+// }
+
+//INHERITANCE(SINGLE INHERITANCE)
+
+// class Animal{
+//     void speaks(){
+//         System.out.println("Animal Speaks..");
+//     }
+// }
+// class Dog extends Animal{
+//     void eats(){
+//         System.out.println("Meat..");
+//     }
+// }
+
+// class Exam{
+//     public static void main (String args[]){
+
+//         Animal a = new Animal();
+//         Dog b = new Dog();
+
+//         a.speaks();
+//         b.eats();
+//     }
+// }
+
+//
+
+
+
+//MULTILEVEL INHERITANCE
+
+
+//cout is overriding you can choose diff name for each class
+// class A{
+//     void cout(){
+//         System.out.println("Grand Parent Class.");
+//     }
+// }
+// class B extends A{
+//     void cout(){
+//         System.out.println("Parent Class");
+//     }
+// }
+// class C extends B{
+//     void cout(){
+//         System.out.println("Child Class");
+//     }
+// }
+
+// class Exam{
+//     public static void main(String args[]){
+//         A a = new A();
+//         B b = new B();
+//         C c = new C();
+
+//         a.cout();
+//         b.cout();
+//         c.cout();
+//     }   
+// }    
+
+// HIERARCHICAL INHERITANCE
+
+// class Parent{
+//     void p(){
+//         System.out.println("Parent Class");
+//     }
+// }
+// class Child extends Parent{
+//     void c(){
+//         System.out.println("First Child Class ");
+//     }
+// }
+// class Child_2 extends Parent{
+//     void c_2(){
+//         System.out.println("Second Child Class");
+//     }
+// }
+
+// class Exam{
+//     public static void main(String args[]){
+//         Parent a = new Parent();
+//         Child b = new Child();
+//         Child_2 c = new Child_2();
+        
+//         a.p();
+//         b.c();
+//         c.c_2();
+//     }
+// }
+
+//
